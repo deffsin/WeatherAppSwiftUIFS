00f135d2e4409f2c07ff145a9e865420d791376e
deffsin <deffsin@gmail.com>
2023-08-02 18:09:47 +0300

    First commit - test

---
+++ WeatherAppSwiftUIFS/Test.swift
@@ -0,0 +1,20 @@
+//
+//  Test.swift
+//  WeatherAppSwiftUIFS
+//
+//  Created by Denis Sinitsa on 02.08.2023.
+//
+
+import SwiftUI
+
+struct Test: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+struct Test_Previews: PreviewProvider {
+    static var previews: some View {
+        Test()
+    }
+}
